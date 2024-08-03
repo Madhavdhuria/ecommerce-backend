3bc0b43ee85addecdfb397d07eca24b0b0e22b8f
Madhavdhuria <madhavdhuria556@gmail.com>
2024-08-03 12:21:00 +0530

    Remove node_modules, dist, and .env from repository

--- dist/routes/coupon.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { newCoupon, applyDiscount, allCoupon, deleteCoupon, } from "../controllers/payment.js";
-import { adminOnly } from "../middlewares/auth.js";
-const app = express.Router();
-app.post("/coupon/new", adminOnly, newCoupon);
-app.get("/discount", applyDiscount);
-app.get("/coupon/all", adminOnly, allCoupon);
-app.delete("/coupon/:id", adminOnly, deleteCoupon);
-export default app;
--- dist/utils/utility-class.js
+++
@@ -1,9 +0,0 @@
-class Errorhandler extends Error {
-    constructor(message, StatusCode) {
-        super(message);
-        this.message = message;
-        this.StatusCode = StatusCode;
-        this.StatusCode = StatusCode;
-    }
-}
-export default Errorhandler;
